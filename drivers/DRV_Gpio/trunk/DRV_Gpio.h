578f35e8bb0a33e953f2cdc60d3611b4c6b5beca
gwendal.le.gall <gwendal.le.gall@0cba0516-ee4b-11dd-901b-0b69d2339e2b>
2010-03-14 13:32:31 +0000

    Add DRV_Gpio to svn repository

---
+++ drivers/DRV_Gpio/trunk/DRV_Gpio.h
@@ -0,0 +1,195 @@
+/**
+*	\file DRV_Gpio.h
+*
+*	\author Gwendal Le Gall
+*
+*	\date 02/02/2010
+*	\brief Public declaration for the driver
+*
+*/
+/*!
+ * \page svn_log SVN log
+ *  \include svn_log.txt
+ */
+/*!
+ * \if standalone_DRV_Gpio
+*	\mainpage GPIO Driver
+*    \else
+*    \page GPIO GPIO Driver
+*    \endif
+*   \author Gwendal Le Gall
+*
+*	\date 13/03/2010
+*
+*	\section Description Description
+*	GPIO Hardware Abstraction layer
+*	\section Configuration Configuration
+*
+*	\section arch architecture specific
+*
+*	\subsection ARM7AT91NoOs ARM7AT91 NoOs
+*
+*	\ref GPIOLinux
+*
+*	\section history History
+*	\ref svn_log
+*/
+/** @defgroup DRV_Gpio_Public_grp Public interface for DRV_Gpio driver
+ *  This is the first group
+ *  @{
+ */
+
+
+#define BOOL unsigned char
+/**************************************************************
+						Typedef
+***************************************************************/
+/*!
+ *  \brief Handle for device
+ */
+typedef void * DRV_Gpio_Handle;
+
+/*!
+ *  \brief Returned error for driver
+ *  \public
+ */
+typedef enum
+{
+	GPIO_No_Error,    //!< OK
+	GPIO_Failed,      //!< generic error
+	GPIO_Device_Not_Found,
+	GPIO_AlreadyOpened,
+	GPIO_Init_Error,
+	GPIO_Bad_Param,
+	GPIO_Unknown      //!< unexpected error
+} DRV_Gpio_Error;
+
+/** @defgroup DRV_Gpio_Settings PIO basics settings
+ *  @ingroup DRV_Gpio_Public_grp
+ *  @{
+ */
+
+/*!
+ *  \brief Type
+ *  \public
+ */
+typedef enum
+{
+        GPIO_Output,         //!<
+        GPIO_Input,         //!<
+        GPIO_IO,       //!<
+        BRUnknown      //!< unexpected error
+} DRV_Gpio_Type;
+
+/*!
+ *  \brief GPIO identification
+ */
+typedef struct
+{
+	char cBank;
+	char cPio;
+} DRV_GpioID;
+
+/*!
+ *  \brief Type
+ *  \public
+ */
+typedef enum
+{
+        GPIO_INT_None,         //!< No interrupt for this pio
+        GPIO_INT_Low2High,     //!< Interrupt on low to high transition
+        GPIO_INT_High2Low,     //!< Interrupt on high to low transition
+        GPIO_INT_Both,			//!< Interrupt on any level transition
+} DRV_Gpio_IntType;
+
+/*!
+ *  \brief GPIO identification
+ */
+typedef struct
+{
+	DRV_Gpio_IntType EventType;
+	void (*CallBack)(void);
+} DRV_GpioINT;
+
+
+
+/*!
+ *  \brief configuration parameters for GPIO
+ */
+typedef struct
+{
+	DRV_GpioID Id;
+	DRV_Gpio_Type Type;
+    unsigned int uiOption;
+    DRV_GpioINT Int
+} DRV_Gpio_Cfg;
+
+/** @} */ //
+/**************************************************************
+					Functions
+***************************************************************/
+/*! \fn DRV_Gpio_Error DRV_Gpio_Init( void )
+ *  \brief Initialization the driver.
+ *  \return Driver error.
+ */
+DRV_Gpio_Error DRV_Gpio_Init( void );
+
+/*! \fn DRV_Gpio_Error DRV_Gpio_Terminate( void )
+ *  \brief Terminate the driver.
+ *  \return Driver error.
+ */
+DRV_Gpio_Error DRV_Gpio_Terminate( void );
+/*! \fn DRV_Gpio_Error DRV_Gpio_Open( DRV_Gpio_Handle *phDeviceHandle, DRV_Gpio_Cfg *ptSettings)
+ *  \brief Open an instance of a GPIO.
+ *  \param phDeviceHandle Handle of the new instance.
+ *  \param ptSettings device settings
+ *  \return Driver error.
+ */
+DRV_Gpio_Error DRV_Gpio_Open( DRV_Gpio_Handle *phDeviceHandle , DRV_Gpio_Cfg *ptSettings);
+/*! \fn DRV_Gpio_Error DRV_Gpio_Close( DRV_Gpio_Handle hDeviceHandle );
+ *  \brief Close an instance of the driver.
+ *  \param hDeviceHandle Handle of the instance to close.
+ *  \return Driver error.
+ */
+DRV_Gpio_Error DRV_Gpio_Close( DRV_Gpio_Handle hDeviceHandle );
+
+/*! \fn BOOL DRV_Gpio_Send( DRV_Gpio_Handle hDeviceHandle);
+ *  \brief Getthe value of a PIO.
+ *  \param hDeviceHandle Handle of the instance to close.
+ *  \return Driver error.
+ */
+BOOL DRV_Gpio_ValueGet( DRV_Gpio_Handle hDeviceHandle );
+
+
+/*! \fn DRV_Gpio_Error DRV_Gpio_Send( DRV_Gpio_Handle hDeviceHandle , BOOL bValue);
+ *  \brief Set a PIO.
+ *  \param hDeviceHandle Handle of the instance to close.
+ *  \return Driver error.
+ */
+DRV_Gpio_Error DRV_Gpio_ValueSet( DRV_Gpio_Handle hDeviceHandle , BOOL bValue);
+
+/*! \fn DRV_Gpio_Error DRV_Gpio_Send( DRV_Gpio_Handle hDeviceHandle ,unsigned char *pucBuffer , int iLength);
+ *  \brief Set the pio type
+ *  \param hDeviceHandle Handle of the instance to close.
+ *  \param eType new type
+ *  \return Driver error.
+ */
+DRV_Gpio_Error DRV_Gpio_TypeSet( DRV_Gpio_Handle hDeviceHandle , DRV_Gpio_Type eType);
+
+/*! \fn DRV_Gpio_Error DRV_Gpio_Send( DRV_Gpio_Handle hDeviceHandle ,unsigned char *pucBuffer , int iLength);
+ *  \brief Set an option
+ *  \param hDeviceHandle Handle of the instance to close.
+ *  \param uiOption Option mask to set
+ *  \return Driver error.
+ */
+DRV_Gpio_Error DRV_Gpio_OptionSet( DRV_Gpio_Handle hDeviceHandle , unsigned int uiOption);
+
+/*! \fn DRV_Gpio_Error DRV_Gpio_Send( DRV_Gpio_Handle hDeviceHandle ,unsigned char *pucBuffer , int iLength);
+ *  \brief reset an option
+ *  \param hDeviceHandle Handle of the instance to close.
+ *  \param uiOption Option mask to reset
+ *  \return Driver error.
+ */
+DRV_Gpio_Error DRV_Gpio_OptionReSet( DRV_Gpio_Handle hDeviceHandle , unsigned int uiOption);
+
+/** @} */ // end of DRV_Gpio_Public_grp
