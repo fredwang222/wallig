ad1296dc036174f3e4cae4bcc1cd25a2132633aa
gwendal.le.gall <gwendal.le.gall@0cba0516-ee4b-11dd-901b-0b69d2339e2b>
2010-04-11 13:10:21 +0000

    [Pwm] Add GPL “copyright” lines and update comments

--- drivers/DRV_Pwm/trunk/Arch/Stm32/DRV_Pwm_Stm32.c
+++ drivers/DRV_Pwm/trunk/Arch/Stm32/DRV_Pwm_Stm32.c
@@ -1,10 +1,40 @@
+/*
+ *    This file is part of Wallig Library and Drivers.
+ *
+ *    Copyright (C) 2010  Gwendal Le Gall
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #include <string.h>
 #include "stm32f10x.h"
 #include "../../DRV_Pwm.h"
 #include "DRV_Pwm_Cfg.h"
 
+/********************************************************************************************************/
+/*							Define																		*/
+/********************************************************************************************************/
 #define PWM_DEVICE_COUNT ( sizeof(PWM_DeviceCfgListe)/sizeof(Pwm_Device_Cfg_t))
 
+/********************************************************************************************************/
+/*							Macro																		*/
+/********************************************************************************************************/
+
+/********************************************************************************************************/
+/*							Typedef																		*/
+/********************************************************************************************************/
 typedef struct
 {
 	GPIO_TypeDef* PORT;
@@ -30,10 +60,20 @@ typedef struct
 
 } Pwm_Device_Data_t;
 
+/********************************************************************************************************/
+/*							Local Variables																*/
+/********************************************************************************************************/
 static const Pwm_Device_Cfg_t PWM_DeviceCfgListe[]=DRV_PWM_INIT_CFG;
 static Pwm_Device_Data_t PWM_DeviceDataListe[PWM_DEVICE_COUNT];
 
-void DRV_Pwmm_GPIO_config( const DRV_Pwm_PioCfg *pGpioCFG);
+/********************************************************************************************************/
+/*							Local functions	declaration													*/
+/********************************************************************************************************/
+void Pwm_GPIO_config( const DRV_Pwm_PioCfg *pGpioCFG);
+
+/********************************************************************************************************/
+/*							Public functions															*/
+/********************************************************************************************************/
 void DRV_Pwm_Init(void )
 {
 	TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
@@ -62,7 +102,7 @@ void DRV_Pwm_Init(void )
 
 }
 
-DRV_Pwm_Error DRV_Pwm_Open( const char *pcName , DRV_Pwm_Handle *pHandle ,const  unsigned short ucDutycycle)
+DRV_Pwm_Error DRV_Pwm_Open( const char *pcName , DRV_Pwm_Handle *pHandle ,  unsigned short ucDutycycle)
 {
 	DRV_Pwm_Error eError = Pwm_Device_Not_Found;
 	int iPwmIndex;
@@ -85,7 +125,7 @@ DRV_Pwm_Error DRV_Pwm_Open( const char *pcName , DRV_Pwm_Handle *pHandle ,const 
 		return Pwm_AlreadyOpened;
 	*pHandle=(DRV_Pwm_Handle) pPwmData;
 	pPwmData->usDutycycle = (PWM_DUTY_CYCLE_FULL*ucDutycycle)/256; ;
-	DRV_Pwmm_GPIO_config(&(pPwmData->pCfg->PioCfg));
+	Pwm_GPIO_config(&(pPwmData->pCfg->PioCfg));
 	/* PWM1 Mode configuration: Channel */
 	TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
 	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
@@ -107,7 +147,7 @@ void DRV_Pwm_Disable( DRV_Pwm_Handle Handle )
 
 }
 
-DRV_Pwm_Error DRV_Pwm_DutyCycleSet(const DRV_Pwm_Handle Handle , const unsigned char ucDutycycle)
+DRV_Pwm_Error DRV_Pwm_DutyCycleSet(const DRV_Pwm_Handle Handle ,  unsigned char ucDutycycle)
 {
 	Pwm_Device_Data_t *pDeviceData =(Pwm_Device_Data_t *)Handle;
 
@@ -120,7 +160,11 @@ DRV_Pwm_Error DRV_Pwm_DutyCycleSet(const DRV_Pwm_Handle Handle , const unsigned 
 
 	return Pwm_No_Error;
 }
-void DRV_Pwmm_GPIO_config( const DRV_Pwm_PioCfg *pGpioCFG)
+
+/********************************************************************************************************/
+/*							Local functions																*/
+/********************************************************************************************************/
+void Pwm_GPIO_config( const DRV_Pwm_PioCfg *pGpioCFG)
 {
 	GPIO_InitTypeDef GPIO_InitStructure;
 	/* Configure alternate output for GPIO*/
--- drivers/DRV_Pwm/trunk/DRV_Pwm.h
+++ drivers/DRV_Pwm/trunk/DRV_Pwm.h
@@ -1,4 +1,27 @@
-#include "DRV_Pwm_Cfg.h"
+/*
+ *    This file is part of Wallig Library and Drivers.
+ *
+ *    Copyright (C) 2010  Gwendal Le Gall
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/********************************************************************************************************/
+/*							Typedef																		*/
+/********************************************************************************************************/
+
 typedef void * DRV_Pwm_Handle;
 
 typedef enum
@@ -18,10 +41,13 @@ typedef enum
 	Pwm_Device_Open,
 } DRV_Pwm_Device_State;
 
+/********************************************************************************************************/
+/*							Public functions Declarations												*/
+/********************************************************************************************************/
 void DRV_Pwm_Init(void );
-DRV_Pwm_Error DRV_Pwm_Open( const char *pcName , DRV_Pwm_Handle *pHandle ,const  unsigned short ucDutycycle);
+DRV_Pwm_Error DRV_Pwm_Open( const char *pcName , DRV_Pwm_Handle *pHandle , unsigned short ucDutycycle);
 void DRV_Pwm_Close( DRV_Pwm_Handle Handle);
 void DRV_Pwm_Enable( DRV_Pwm_Handle Handle );
 void DRV_Pwm_Disable( DRV_Pwm_Handle Handle );
-DRV_Pwm_Error DRV_Pwm_DutyCycleSet(const DRV_Pwm_Handle Handle , const unsigned char ucDutycycle);
+DRV_Pwm_Error DRV_Pwm_DutyCycleSet(const DRV_Pwm_Handle Handle ,  unsigned char ucDutycycle);
 
