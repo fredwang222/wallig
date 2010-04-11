9c83e44dda759f3ada09624b0a28a03c2e048684
gwendal.le.gall <gwendal.le.gall@0cba0516-ee4b-11dd-901b-0b69d2339e2b>
2010-04-11 12:54:33 +0000

    [Quad] Add GPL “copyright” lines

--- drivers/DRV_Quad/trunk/DRV_Quad.h
+++ drivers/DRV_Quad/trunk/DRV_Quad.h
@@ -1,4 +1,26 @@
-#include "DRV_Quad_Cfg.h"
+/*
+ *    This file is part of Wallig Library and Drivers.
+ *
+ *    Copyright (C) 2010  Gwendal Le Gall
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/********************************************************************************************************/
+/*							Typedef																		*/
+/********************************************************************************************************/
 typedef void * DRV_Quad_Handle;
 
 typedef enum
@@ -18,6 +40,9 @@ typedef enum
 	Quad_Device_Open,
 } DRV_Quad_Device_State;
 
+/********************************************************************************************************/
+/*							Public functions Declarations												*/
+/********************************************************************************************************/
 void DRV_Quad_Init(void );
 DRV_Quad_Error DRV_Quad_Open( char *pcName , DRV_Quad_Handle *pHandle);
 void DRV_Quad_Close( DRV_Quad_Handle Handle);
