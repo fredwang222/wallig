5b38e170954df782b4d1f33dfc41d55c785f130f
gwendal.le.gall <gwendal.le.gall@0cba0516-ee4b-11dd-901b-0b69d2339e2b>
2010-08-14 12:50:00 +0000

    [LIB_Fifo] minor changes, add GPL header

--- library/LIB_Fifo/trunk/LIB_Fifo.h
+++ library/LIB_Fifo/trunk/LIB_Fifo.h
@@ -1,3 +1,30 @@
+/*
+ * 	 Author : Gwendal Le Gall
+ *   Date 02/02/2010
+ *
+*    Modified from:
+ *     - nRF24l01 driver of Neil MacMillan see: http://code.google.com/p/nrf24l01/
+ *     - http://www.tinkerer.eu/AVRLib/nRF24L01
+
+ *
+ *    This file is part of Wallig Library and Drivers.
+ *
+ *    Copyright (C) 2010  Gwendal Le Gall
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 #ifndef LIB_FIFO_H
 #define LIB_FIFO_H
 
