21357a4a31b24a3acff176aebe5c4ee9da6c5186
gwendal.le.gall <gwendal.le.gall@0cba0516-ee4b-11dd-901b-0b69d2339e2b>
2010-11-01 13:32:11 +0000

    [Template] update Makefile

--- Projects/Template/trunk/Script/createmakefile.sh
+++ Projects/Template/trunk/Script/createmakefile.sh
@@ -2,11 +2,15 @@
 MAKFILE=Makefile
 echo "#$MAKFILE generated with prjmake.sh" >$MAKFILE
 echo "">>$MAKFILE
+echo 'export PROJECT_PATH=$(CURDIR)'>>$MAKFILE
+echo 'export CONFIG_PATH=Config'>>$MAKFILE
 echo "######################################################################">>$MAKFILE
-echo "#system include">>$MAKFILE
+echo "# system variables">>$MAKFILE
 echo "######################################################################">>$MAKFILE
-echo "include  System/"$ARCH"/arch.mk">>$MAKFILE
+echo "export ARCH_PATH=System/"$ARCH>>$MAKFILE
+echo 'include  $(ARCH_PATH)/arch.mk'>>$MAKFILE
 echo "">>$MAKFILE
+
 echo "######################################################################">>$MAKFILE
 echo "# driver variables">>$MAKFILE
 echo "######################################################################">>$MAKFILE
@@ -15,7 +19,7 @@ echo "export DRV_PATH="$DRV_PATH>>$MAKFILE
 for DRV in $DRVIER_LIST 
 do 
 echo "#"$DRV>>$MAKFILE
-echo $DRV'_PATH=$(DRV_PATH)/'$DRV>>$MAKFILE
+echo "export "$DRV'_PATH=$(DRV_PATH)/'$DRV>>$MAKFILE
 echo $DRV'_OBJ=$(wildcard $('$DRV'_PATH)/obj/*.o)'>>$MAKFILE
 done
 echo "">>$MAKFILE
@@ -28,7 +32,7 @@ echo "export LIB_PATH="$LIB_PATH>>$MAKFILE
 for LIB in $LIB_LIST 
 do 
 echo "#"$LIB>>$MAKFILE
-echo $LIB'_PATH=$(DRV_PATH)/'$LIB>>$MAKFILE
+echo "export "$LIB'_PATH=$(LIB_PATH)/'$LIB>>$MAKFILE
 echo $LIB'_OBJ=$(wildcard $('$LIB'_PATH)/obj/*.o)'>>$MAKFILE
 done
 echo "">>$MAKFILE
@@ -40,6 +44,23 @@ echo "#project">>$MAKFILE
 echo 'PROJECT_SRC=$(wildcard src/*.c)'>>$MAKFILE
 echo 'PROJECT_OBJ:=$(patsubst %.c,%.o,$(PROJECT_SRC))'>>$MAKFILE
 echo "">>$MAKFILE
+
+
+echo "######################################################################">>$MAKFILE
+echo "# inlcude dir">>$MAKFILE
+echo "######################################################################">>$MAKFILE
+echo -n "export IDIR=-I. -I.. ">>$MAKFILE
+for DRV in $DRVIER_LIST 
+do 
+echo -n '-I$(PROJECT_PATH)/$('$DRV'_PATH) '>>$MAKFILE
+done
+for LIB in $LIB_LIST 
+do 
+echo -n '-I$(PROJECT_PATH)/$('$LIB'_PATH) '>>$MAKFILE
+done
+echo -n '-I$(PROJECT_PATH)/$(ARCH_PATH)/lib/inc '>>$MAKFILE
+echo " ">>$MAKFILE
+echo " ">>$MAKFILE
 
 echo "######################################################################">>$MAKFILE
 echo "#all target">>$MAKFILE
--- Projects/Template/trunk/prjmake.sh
+++ Projects/Template/trunk/prjmake.sh
@@ -14,7 +14,7 @@ SVN_ACCESS=https
 else
 SVN_ACCESS=http
 fi
-
+echo ""
 echo "----------------------------------------------"
 #get drivers
 export DRVIER_LIST=`grep DRV_ prj.tmp`
@@ -27,8 +27,8 @@ do
 	then 
 		if [ ! -e $DRV_PATH/$DRV ]
 		then
-			echo "get $DRV"
-			svn co $SVN_ACCESS"://"`grep $DRV $DRV_PATH/Drv.conf | sed s/.*URL=//` $DRV_PATH/$DRV
+			echo "    get $DRV"
+			svn co $SVN_ACCESS"://"`grep ^$DRV $DRV_PATH/Drv.conf | sed s/.*URL=//` $DRV_PATH/$DRV
 		else
 			echo "    $DRV already present"
 		fi
@@ -36,7 +36,7 @@ do
 		echo "    $DRV not found"
 	fi
 done
-
+echo ""
 echo "----------------------------------------------"
 export LIB_LIST=`grep LIB_ prj.tmp`
 echo "Lib List:" $LIB_LIST
@@ -47,8 +47,8 @@ do
 	then 
 		if [ ! -e Lib/$LIB ]
 		then
-			echo "get $LIB"
-			svn co $SVN_ACCESS"://"`grep $LIB Lib/Lib.conf | sed s/.*URL=//` Lib/$LIB
+			echo "    get $LIB"
+			svn co $SVN_ACCESS"://"`grep ^$LIB Lib/Lib.conf | sed s/.*URL=//` Lib/$LIB
 		else
 			echo "    $LIB already present"
 		fi
@@ -56,15 +56,21 @@ do
 		echo "    $LIB not found"
 	fi
 done
-
+echo ""
 echo "----------------------------------------------"
 echo "get sytem"
 export ARCH=`grep $TARGET System/System.conf | sed s/.*ARCH=//`
 echo "    Arch: "$ARCH
-#if [ ! -e Lib/$LIB ]
-#then
-#fi
-
+if grep -q $ARCH System/System.conf
+then
+	if [ ! -e System/$ARCH ]
+	then
+		svn co $SVN_ACCESS"://"`grep ^$ARCH System/System.conf | sed s/.*URL=//` System/$ARCH
+	fi
+else
+	echo "    $ARCH not found"
+fi
+echo ""
 echo "----------------------------------------------"
 echo "create Makefile"
 Script/createmakefile.sh
