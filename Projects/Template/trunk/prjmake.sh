a0ab3f8e58aae33dfad4fd455e26d8ebf4266e44
gwendal.le.gall <gwendal.le.gall@0cba0516-ee4b-11dd-901b-0b69d2339e2b>
2010-11-01 11:30:28 +0000

    [Template] add driver and lib checkout

--- Projects/Template/trunk/prjmake.sh
+++ Projects/Template/trunk/prjmake.sh
@@ -13,4 +13,41 @@ echo "Driver List:" $DRVIER_LIST
 #get lib
 LIB_LIST=`grep LIB_ prj.tmp`
 echo "Lib List:" $LIB_LIST
+#svn access type
+if [[ `grep SVN_HTTP prj.tmp` = "SVN_HTTPS" ]]
+then
+SVN_ACCESS=https
+else
+SVN_ACCESS=http
+fi
+echo $SVN_ACCESS
 
+#get drivers
+for DRV in $DRVIER_LIST 
+do 
+	if grep -q $DRV Drv/Drv.conf
+	then 
+		if [ ! -e Drv/$DRV ]
+		then
+			echo "get $DRV"
+			svn co $SVN_ACCESS"://"`grep $DRV Drv/Drv.conf | sed s/.*URL=//` Drv/$DRV
+		fi
+	else
+		echo "$DRV not found"
+	fi
+done
+
+#get Lib
+for LIB in $LIB_LIST 
+do 
+	if grep -q $LIB Lib/Lib.conf
+	then 
+		if [ ! -e Lib/$LIB ]
+		then
+			echo "get $LIB"
+			svn co $SVN_ACCESS"://"`grep $LIB Lib/Lib.conf | sed s/.*URL=//` Lib/$LIB
+		fi
+	else
+		echo "$LIB not found"
+	fi
+done
