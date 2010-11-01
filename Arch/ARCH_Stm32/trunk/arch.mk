d31673f94d24f59e4476a476b566021866db2e67
gwendal.le.gall <gwendal.le.gall@0cba0516-ee4b-11dd-901b-0b69d2339e2b>
2010-11-01 16:08:34 +0000

    [ARCH_Stm32] add file for template

--- Arch/ARCH_Stm32/trunk/arch.mk
+++ Arch/ARCH_Stm32/trunk/arch.mk
@@ -1,3 +1,3 @@
 export CC=arm-none-eabi-gcc 
-export CFLAGS=-DARCH_STM32 -DSTM32F10X_MD -mthumb   -mcpu=cortex-m3 -mfix-cortex-m3-ldrd -O0 -g3  -Wall -MMD -MP -MF$(patsubst %.o,%.d,$@) -MT$(patsubst %.o,%.d,$@) -Wa,-adhlns=$(patsubst %.o,%.lst,$@)
-export LDFLAGS=-Wl,--gc-sections,-Map=$(patsubst %.elf,%.map,$@),-cref,-u,Reset_Handler -L$(ARCH_INCLUDE_PATH)/lib -T $(ARCH_PATH)/stm32.ld -mthumb -march=armv7 -mfix-cortex-m3-ldrd
+export CFLAGS=-DARCH_STM32  -mthumb $(ARCH_DEFINE)  -mcpu=cortex-m3 -mfix-cortex-m3-ldrd -O0 -g3  -Wall -MMD -MP -MF$(patsubst %.o,%.d,$@) -MT$(patsubst %.o,%.d,$@) -Wa,-adhlns=$(patsubst %.o,%.lst,$@)
+export LDFLAGS=-Wl,--gc-sections,-Map=$(patsubst %.elf,%.map,$@),-cref,-u,Reset_Handler -L$(ARCH_PATH)/lib -T $(ARCH_PATH)/stm32.ld -mthumb -march=armv7 -mfix-cortex-m3-ldrd $(ARCH_PATH)/lib/libstm32.a
