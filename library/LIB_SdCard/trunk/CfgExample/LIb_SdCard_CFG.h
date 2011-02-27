98967aa9bd7096a2df1a74cd4262c0f67c1bacc0
gwendal.le.gall@gmail.com <gwendal.le.gall@gmail.com@0cba0516-ee4b-11dd-901b-0b69d2339e2b>
2011-02-27 12:53:30 +0000

    [LIB_SdCard] add config file example

---
+++ library/LIB_SdCard/trunk/CfgExample/LIb_SdCard_CFG.h
@@ -0,0 +1,13 @@
+/*
+ * LIb_SdCard_CFG.h
+ *
+ *  Created on: 27 févr. 2011
+ *      Author: cara
+ */
+
+#ifndef LIB_SDCARD_CFG_H_
+#define LIB_SDCARD_CFG_H_
+
+#define SDCARD_CFG_INIT { "SPISD" , "SD_CS" , "SD_WP" , "SD_CD"}
+
+#endif /* LIB_SDCARD_CFG_H_ */
