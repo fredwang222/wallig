2ac7c5f70ba8bc2505ab38f2ad0ad7a5da8484ff
gwendal.le.gall@gmail.com <gwendal.le.gall@gmail.com@0cba0516-ee4b-11dd-901b-0b69d2339e2b>
2012-01-14 18:47:45 +0000

    [ARCH_Stm32] add stm32f2xx support

---
+++ Arch/ARCH_Stm32/trunk/inc_f1/System.h
@@ -0,0 +1,14 @@
+#ifdef ARCH_SAM7
+#include "arch/ARCH_Sam7/ARCH_SAM7_def.h"
+#endif
+
+#ifdef ARCH_LINUX
+#include "arch/ARCH_Linux/ARCH_Linux_def.h"
+#endif
+
+#ifdef ARCH_STM32
+#include "lib/inc/stm32f10x.h"
+#include "ARCH_Stm32_system.h"
+#endif
+
+void System_init( void );
