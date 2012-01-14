a5a272af806c8cf4a3a0c164c203ddf8b1be6581
gwendal.le.gall@gmail.com <gwendal.le.gall@gmail.com@0cba0516-ee4b-11dd-901b-0b69d2339e2b>
2012-01-14 23:51:56 +0000

    [DRV_Uart] Add Stm32f2xx support

--- drivers/DRV_Uart/trunk/Arch/Stm32/SetConfig.sh
+++ drivers/DRV_Uart/trunk/Arch/Stm32/SetConfig.sh
@@ -11,8 +11,14 @@ echo '#define kUART1_COM_PARAM { '`Get_Value CONFIG_DRV_UART1_BAUDRATE`' , '\
 								 ''`Get_Value CONFIG_DRV_UART1_STOPBITS`' , '\
 								 ''`Get_Value CONFIG_DRV_UART1_PARITY`' , '\
 								'USART_Mode_Rx | USART_Mode_Tx , USART_HardwareFlowControl_None }' > $CONFIG_PATH/DRV_Uart_CFG.h
+if [ $SUB_ARCH != "SUB_STM32F2XX" ]
+then
 echo '#define kUART1_CLK_DEV_PARAM { RCC_APB2PeriphClockCmd , RCC_APB2Periph_USART1| RCC_APB2Periph_AFIO }' >> $CONFIG_PATH/DRV_Uart_CFG.h
 echo '#define kUART1_CLK_PIO_PARAM { RCC_APB2PeriphClockCmd , RCC_APB2Periph_GPIOA  | RCC_APB2Periph_AFIO  }' >> $CONFIG_PATH/DRV_Uart_CFG.h
+else
+echo '#define kUART1_CLK_DEV_PARAM { RCC_APB2PeriphClockCmd , RCC_APB2Periph_USART1 }' >> $CONFIG_PATH/DRV_Uart_CFG.h
+echo '#define kUART1_CLK_PIO_PARAM { RCC_AHB1PeriphClockCmd , RCC_AHB1Periph_GPIOA  }' >> $CONFIG_PATH/DRV_Uart_CFG.h
+fi
 echo '#define kUART1_PIO_PARAM { GPIOA , GPIO_Pin_9 , GPIO_Pin_10 }' >> $CONFIG_PATH/DRV_Uart_CFG.h
 echo '#define kUART1_CONF { "UART1" , USART1,  kUART1_COM_PARAM , kUART1_CLK_DEV_PARAM , kUART1_CLK_PIO_PARAM , kUART1_PIO_PARAM , USART1_IRQn }' >> $CONFIG_PATH/DRV_Uart_CFG.h
 
