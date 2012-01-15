f91d2fc38845aeae0754b6bf509dd00c1f17a26f
gwendal.le.gall@gmail.com <gwendal.le.gall@gmail.com@0cba0516-ee4b-11dd-901b-0b69d2339e2b>
2012-01-15 12:07:24 +0000

    [Template] add driver include

--- Projects/Template/trunk/Drv/Drv.sh
+++ Projects/Template/trunk/Drv/Drv.sh
@@ -2,15 +2,18 @@
 echo "genrate Drv.mk && Makefile"
 grep "#" -v $PRJ_CONFIG_FILE | grep -v ^$$ | sed s/CONFIG_// | grep DRV_ > conf.txt
 echo "export DRV_OBJ :=" > Drv.mk
+echo "export DRV_INC :=" >> Drv.mk
 echo "all:" > Makefile
 if grep -q "DRV_UART=y" $PRJ_CONFIG_FILE
 then
 echo "DRV_OBJ += Drv/Uart/obj/DRV_Uart.a" >> Drv.mk
+echo "DRV_INC += -I Drv/Uart/" >> Drv.mk
 echo '	@(cd Uart && $(MAKE))' >> Makefile
 fi
 if grep -q "DRV_GPIO=y" $PRJ_CONFIG_FILE
 then
 echo "DRV_OBJ += Drv/Gpio/obj/DRV_Gpio.a" >> Drv.mk
+echo "DRV_INC += -I Drv/Gpio/" >> Drv.mk
 echo '	@(cd Gpio && $(MAKE))' >> Makefile
 fi
 
