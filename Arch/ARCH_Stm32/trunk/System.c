efc2f23b4fedf5dc13e2dea1276ea40ed8a5983c
gwendal.le.gall@gmail.com <gwendal.le.gall@gmail.com@0cba0516-ee4b-11dd-901b-0b69d2339e2b>
2012-01-24 19:55:13 +0000

    [ARCH_Stm32] Add System.c file with libc extern functions

---
+++ Arch/ARCH_Stm32/trunk/System.c
@@ -0,0 +1,58 @@
+#include "stdlib.h"
+#include "System.h"
+
+#ifdef  USE_FULL_ASSERT
+
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{ 
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+#endif
+
+void* _sbrk (int incr)
+{
+#ifdef DATA_IN_ExtSRAM
+	static char *heap_end = (char*)0x68000000;
+#else
+    static char *heap_end=&end;		/* Defined by the linker */
+#endif
+    char *prev_heap_end;
+
+  prev_heap_end = heap_end;
+  if (heap_end + incr > __get_MSP())
+    {
+      assert_failed(__FILE__,__LINE__);
+    }
+  heap_end += incr;
+  return (void *) prev_heap_end;
+}
+
+
+int  _kill( int b, int c)
+{
+	return 0;
+}
+
+int _getpid( void )
+{
+	return 1;
+}
+
+void _exit( void )
+{
+
+}
+
