d376595c32e8a4bb5d8ae3a315969c8a907f6ac2
gwendal.le.gall@gmail.com <gwendal.le.gall@gmail.com@0cba0516-ee4b-11dd-901b-0b69d2339e2b>
2012-03-04 13:20:29 +0000

    [Template] add LIb_Mp3

--- Projects/Template/trunk/Lib/Lib.sh
+++ Projects/Template/trunk/Lib/Lib.sh
@@ -1,20 +1,51 @@
 #!/bin/bash
 echo "genrate Lib.mk && Makefile"
 grep "#" -v $PRJ_CONFIG_FILE | grep -v ^$$ | sed s/CONFIG_// | grep LIB_ > conf.txt
+########### Obj and lib list ################
 echo "export LIB_OBJ :=" > Lib.mk
 echo "export LIB_INC :=" >> Lib.mk
-echo "all:" > Makefile
+#fifo
 if grep -q "LIB_FIFO=y" $PRJ_CONFIG_FILE
 then
-echo "LIB_OBJ += Lib/Uart/obj/LIB_Fifo.a" >> Lib.mk
-echo "LIB_INC += -I Lib/Uart/" >> Lib.mk
-echo '	@(cd Uart && $(MAKE))' >> Makefile
+echo "LIB_OBJ += Lib/Fifo/LIB_Fifo.o" >> Lib.mk
+echo "LIB_INC += -I Lib/Fifo/" >> Lib.mk
+fi
+#Mp3
+if grep -q "LIB_MP3=y" $PRJ_CONFIG_FILE
+then
+echo "LIB_OBJ += Lib/Mp3/libmp3.a" >> Lib.mk
+echo "LIB_INC += -I Lib/Mp3/" >> Lib.mk
 fi
 echo "" >> Makefile
+######### all target ###########
+echo "all:" > Makefile
+#fifo
+if grep -q "LIB_FIFO=y" $PRJ_CONFIG_FILE
+then
+echo '	@(cd Fifo && $(MAKE))' >> Makefile
+fi
+#Mp3
+if grep -q "LIB_MP3=y" $PRJ_CONFIG_FILE
+then
+echo '	@(cd Mp3 && $(MAKE))' >> Makefile
+fi
+echo "" >> Makefile
+######### clean target ###########
 echo "clean:" >> Makefile
 if grep -q "LIB_FIFO=y" $PRJ_CONFIG_FILE
 then
-echo '	@(cd Uart && $(MAKE) clean)' >> Makefile
+echo '	@(cd Fifo && $(MAKE) clean)' >> Makefile
+fi
+if grep -q "LIB_MP3=y" $PRJ_CONFIG_FILE
+then
+echo '	@(cd Mp3 && $(MAKE) clean)' >> Makefile
+fi
+
+###### Mp3 Lib configuration ######
+if grep -q "LIB_MP3=y" $PRJ_CONFIG_FILE
+then
+cd Mp3
+./SetConfig.sh
 fi
 
 #if grep -q "LIB_FIFO=y" $PRJ_CONFIG_FILE
