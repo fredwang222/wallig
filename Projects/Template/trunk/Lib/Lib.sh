e3dba64f54ad71b842309be3a6735c48a49f5c2c
gwendal.le.gall@gmail.com <gwendal.le.gall@gmail.com@0cba0516-ee4b-11dd-901b-0b69d2339e2b>
2012-03-17 16:58:24 +0000

    [Template] add Ntl support

--- Projects/Template/trunk/Lib/Lib.sh
+++ Projects/Template/trunk/Lib/Lib.sh
@@ -8,13 +8,19 @@ echo "export LIB_INC :=" >> Lib.mk
 if grep -q "LIB_FIFO=y" $PRJ_CONFIG_FILE
 then
 echo "LIB_OBJ += Lib/Fifo/LIB_Fifo.o" >> Lib.mk
-echo "LIB_INC += -I Lib/Fifo/" >> Lib.mk
+echo 'LIB_INC += -I $(PROJECT_PATH)/Lib/Fifo/' >> Lib.mk
 fi
 #Mp3
 if grep -q "LIB_MP3=y" $PRJ_CONFIG_FILE
 then
 echo "LIB_OBJ += Lib/Mp3/libmp3.a" >> Lib.mk
-echo "LIB_INC += -I Lib/Mp3/" >> Lib.mk
+echo 'LIB_INC += -I $(PROJECT_PATH)/Lib/Mp3/' >> Lib.mk
+fi
+#Ntl
+if grep -q "LIB_NTL=y" $PRJ_CONFIG_FILE
+then
+echo "LIB_OBJ += Lib/Mp3/libntl.a" >> Lib.mk
+echo 'LIB_INC += -I $(PROJECT_PATH)/Lib/Ntl/' >> Lib.mk
 fi
 echo "" >> Makefile
 ######### all target ###########
@@ -29,6 +35,11 @@ if grep -q "LIB_MP3=y" $PRJ_CONFIG_FILE
 then
 echo '	@(cd Mp3 && $(MAKE))' >> Makefile
 fi
+#Ntl
+if grep -q "LIB_NTL=y" $PRJ_CONFIG_FILE
+then
+echo '	@(cd Ntl && $(MAKE))' >> Makefile
+fi
 echo "" >> Makefile
 ######### clean target ###########
 echo "clean:" >> Makefile
@@ -40,14 +51,23 @@ if grep -q "LIB_MP3=y" $PRJ_CONFIG_FILE
 then
 echo '	@(cd Mp3 && $(MAKE) clean)' >> Makefile
 fi
-
+if grep -q "LIB_NTL=y" $PRJ_CONFIG_FILE
+then
+echo '	@(cd Ntl && $(MAKE) clean)' >> Makefile
+fi
 ###### Mp3 Lib configuration ######
 if grep -q "LIB_MP3=y" $PRJ_CONFIG_FILE
 then
 cd Mp3
 ./SetConfig.sh
+cd ..
 fi
-
+###### NTL Lib configuration ######
+if grep -q "LIB_NTL=y" $PRJ_CONFIG_FILE
+then
+cd Ntl
+./SetConfig.sh
+fi
 #if grep -q "LIB_FIFO=y" $PRJ_CONFIG_FILE
 #then
 #cd Fifo
