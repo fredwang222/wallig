ccd0fd882b43f495bc5ce902e4705e81e0d190ed
gwendal.le.gall@gmail.com <gwendal.le.gall@gmail.com@0cba0516-ee4b-11dd-901b-0b69d2339e2b>
2012-03-18 10:41:19 +0000

    [Template] use single quote

--- Projects/Template/trunk/Drv/Drv.sh
+++ Projects/Template/trunk/Drv/Drv.sh
@@ -7,20 +7,20 @@ echo "all:" > Makefile
 if grep -q "DRV_UART=y" $PRJ_CONFIG_FILE
 then
 echo "DRV_OBJ += Drv/Uart/obj/DRV_Uart.a" >> Drv.mk
-echo "DRV_INC += -I Drv/Uart/" >> Drv.mk
+echo 'DRV_INC += -I $(PROJECT_PATH)/Drv/Uart/' >> Drv.mk
 echo '	@(cd Uart && $(MAKE))' >> Makefile
 fi
 if grep -q "DRV_GPIO=y" $PRJ_CONFIG_FILE
 then
 echo "DRV_OBJ += Drv/Gpio/obj/DRV_Gpio.a" >> Drv.mk
-echo "DRV_INC += -I Drv/Gpio/" >> Drv.mk
+echo 'DRV_INC += -I $(PROJECT_PATH)/Drv/Gpio/' >> Drv.mk
 echo '	@(cd Gpio && $(MAKE))' >> Makefile
 fi
 
 if grep -q "DRV_NAND=y" $PRJ_CONFIG_FILE
 then
 echo "DRV_OBJ += Drv/Nand/obj/DRV_Nand.o" >> Drv.mk
-echo "DRV_INC += -I Drv/Nand/" >> Drv.mk
+echo 'DRV_INC += -I $(PROJECT_PATH)/Drv/Nand/' >> Drv.mk
 echo '	@(cd Nand && $(MAKE))' >> Makefile
 fi
 
